d49fe81ff134eaf98284e86c207cf80a8338d8f4
rohit <codyyrohit@gmail.com>
2024-10-17 22:26:51 +0530

    Final Tailwind UI Touch

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,17 +23,17 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-gradient-to-r from-teal-100 to-blue-100 shadow-lg'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-teal-600'>👨🏻‍⚕Health</span>
+            <span className='text-blue-800'>Sphere</span>
           </h1>
         </Link>
         <form
           onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
+          className='bg-white p-3 rounded-full shadow-sm flex items-center'
         >
           <input
             type='text'
@@ -43,17 +43,17 @@ export default function Header() {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
-            <FaSearch className='text-slate-600' />
+            <FaSearch className='text-teal-600' />
           </button>
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-blue-800 hover:text-teal-600 transition-colors duration-300'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-blue-800 hover:text-teal-600 transition-colors duration-300'>
               About
             </li>
           </Link>
@@ -65,7 +65,7 @@ export default function Header() {
                 alt='profile'
               />
             ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              <li className='text-blue-800 hover:text-teal-600 transition-colors duration-300'> Sign in</li>
             )}
           </Link>
         </ul>
@@ -73,3 +73,6 @@ export default function Header() {
     </header>
   );
 }
+
+
+
